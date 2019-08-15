6016ebee2410ac2a8c15610f72d96f98d9680e8c
Antonio M S S Jr <amarcosju@gmail.com>
2019-08-15 11:21:59 -0300

    Update TestExtensions.cs

--- Bank.Entries.Tests/TestExtensions.cs
+++ Bank.Entries.Tests/TestExtensions.cs
@@ -13,24 +13,25 @@ namespace Bank.Entries.Tests
     public static class MockExtensions
     {
         public static ISetup<T, T2> SetupOn<T, T2>(this Expression<Func<T, T2>> expr, Mock<T> mock) where T : class
-        {
-            return mock.Setup(expr);
-        }
+            => mock.Setup(expr);
+
+        public static ISetupSequentialResult<T2> SetupSequenceOn<T, T2>(this Expression<Func<T, T2>> expr, Mock<T> mock) where T : class
+            => mock.SetupSequence(expr);
 
         public static ISetup<T> SetupOn<T>(this Expression<Action<T>> expr, Mock<T> mock) where T : class
-        {
-            return mock.Setup(expr);
-        }
+            => mock.Setup(expr);
 
         public static void VerifyOn<T, T2>(this Expression<Func<T, T2>> expr, Mock<T> mock, Times times) where T : class
-        {
-            mock.Verify(expr, times);
-        }
+            => mock.Verify(expr, times);
 
         public static void VerifyOn<T>(this Expression<Action<T>> expr, Mock<T> mock, Times times) where T : class
-        {
-            mock.Verify(expr, times);
-        }
+            => mock.Verify(expr, times);
+
+        public static IEnumerable<IInvocation> GetInvocationsByName<T>(this Mock<T> mock, string methodName) where T : class
+            => mock.Invocations.Where(a => a.Method.Name == methodName);
+
+        public static T TryGetArgument<T>(this IInvocation invocation) where T : class
+            => invocation.Arguments.FirstOrDefault(arg => arg is T) as T;
 
     }
 }
